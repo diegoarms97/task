8aadefa5332f0a4e384a394399ed9a3ff7cefbb6
diego <gcuenta05@gmail.com>
2022-07-25 02:29:09 -0500

    funcion sumar y clase autos

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumar(45, 54));
+    }
+    private static int sumar(int a, int b){
+        int r=a+ 87;
+        return r;
+    }
+}
